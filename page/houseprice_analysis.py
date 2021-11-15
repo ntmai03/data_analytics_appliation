6aa2849a9ff6794e26eaf2307c112162f824e86a
ntmai03 <36872488+ntmai03@users.noreply.github.com>
2021-11-14 20:56:59 -0800

    Add files via upload

--- page/houseprice_analysis.py
+++ page/houseprice_analysis.py
@@ -1,5 +1,21 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import joblib
 
+import sys
+assert sys.version_info >= (3, 5)
+from pathlib import Path
+import os
+
+
+PROJECT_ROOT_DIR =  Path(__file__).resolve().parents[1]
+# source code
+FUNCTION_PATH = os.path.join(PROJECT_ROOT_DIR, "src")
+sys.path.append(FUNCTION_PATH)
+
+from src import config as cf
+from src.util import data_manager as dm
 
 def app():
 	st.sidebar.subheader('Select function')
@@ -12,4 +28,8 @@ def app():
 	st.sidebar.header('')
 
 	if task_option == 'Introduction':
-		st.write('Comming soon...')
+		st.write(cf.DATA_RAW_PATH)
+		data_file = os.path.join(cf.DATA_RAW_PATH, "kc_house_data.csv")
+		df = dm.load_csv_data(data_file)
+		st.write("#### First 100 rows")
+		st.write(df.head(100))
