39527de16ddb9e0c70149786c5dea7e655e7ca67
ntmai03 <36872488+ntmai03@users.noreply.github.com>
2021-11-18 20:41:00 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,15 +8,35 @@ from multipage import MultiPage
 from page import introduction
 from page import houseprice_analysis
 from page import diabetes_analysis
+from page import time_series
 
 
 # Config layout
 st.set_page_config(layout="wide", initial_sidebar_state="expanded")
-display = Image.open('image/common/Logo.png')
+display = Image.open('image/common/DataScienceBanner.jpg')
 display = np.array(display)
-col1, col2 = st.columns(2)
-col1.image(display, width = 400)
-col2.title('Data Analytics Application')
+#st.image(display, width=800)
+#st.image(display)
+
+
+col1, col2, col3 = st.columns([1.5,6,1.5])
+
+with col1:
+	st.write("")
+
+with col2:
+	st.image(display)
+
+with col3:
+	st.markdown("written by [Mai Nguyen](https://www.linkedin.com/in/ntmai03/)")
+
+#col1, col2 = st.columns(2)
+#col1.image(display, width = 400)
+#new_title = '<p style="font-family:sans-serif; color:LightGreen; font-size: 42px;">Data Analytics Application</p>'
+#col2.markdown(new_title, unsafe_allow_html=True)
+
+
+#col2.header('Data Analytics Application')
 
 # Create an instance of the app
 app = MultiPage()
@@ -25,6 +45,7 @@ app = MultiPage()
 app.add_page("Select Application", introduction.app)
 app.add_page("01-House Price Analysis", houseprice_analysis.app)
 app.add_page("02-Diabetes Analysis", diabetes_analysis.app)
+app.add_page("03-Time Series Analysis", time_series.app)
 
 # The main app
 app.run()
