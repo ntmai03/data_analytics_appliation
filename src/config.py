59827f682959e69684323435eac144d44a6d4801
ntmai03 <36872488+ntmai03@users.noreply.github.com>
2021-11-18 21:04:15 -0800

    Update config.py

--- src/config.py
+++ src/config.py
@@ -29,16 +29,3 @@ S3_DATA_RAW_PATH = 'raw/'
 
 def test_funct():
 	st.write('test')
-
-"""
-S3_CLIENT = boto3.client(
-    's3',
-    aws_access_key_id = st.secrets["aws_access_key_id"],
-    aws_secret_access_key = st.secrets["aws_secret_access_key"],
-    region_name = st.secrets["region_name"]
-)
-
-aws_access_key_id = "AKIAZFGIKEZ3JECBN5MV"
-aws_secret_access_key = "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"
-region_name = "us-east-2"
-"""
