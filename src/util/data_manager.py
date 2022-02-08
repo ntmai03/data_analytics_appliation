b80e60ee325d0c4e624c8dbe6004e73fd3239f23
ntmai03 <36872488+ntmai03@users.noreply.github.com>
2022-02-08 01:03:04 -0800

    Add files via upload

--- src/util/data_manager.py
+++ src/util/data_manager.py
@@ -1,7 +1,11 @@
 import sys
 import os
+from io import StringIO
+import json
+import csv
 from pathlib import Path
 import boto3
+from botocore.exceptions import ClientError
 
 import pandas as pd
 # split data
@@ -35,6 +39,61 @@ def s3_load_csv(bucket, filename):
     )   
     # Read data from the S3 object
     return pd.read_csv(obj['Body'])
+
+
+
+# To check whether root bucket exists or not
+def bucket_exists(bucket_name, s3_resource):
+    try:
+        s3_resource.meta.client.head_bucket(Bucket=bucket_name)
+        # print("Bucket exists.", bucket_name)
+        exists = True
+    except ClientError as error:
+        error_code = int(error.response['Error']['Code'])
+        if error_code == 403:
+            print("Private Bucket. Forbidden Access! ", bucket_name)
+        elif error_code == 404:
+            #print("Bucket Does Not Exist!", bucket_name)
+            exists = False
+    
+    return exists
+
+
+# create a new bucket on S3
+def create_s3_bucket(bucket_name, s3_resource):
+    results = s3_resource.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={'LocationConstraint': cf.S3_LOCATION_CONSTRAINT}
+    )
+
+
+# store a json file
+def write_json_file(bucket_name, file_name, data, type='s3'):
+    if(type == 's3'):
+        cf.S3_CLIENT.put_object(Bucket=bucket_name, Key=file_name, Body = json.dumps(data).encode('UTF-8'))        
+    elif(type == 'local'):
+        with open(file_name, 'w') as outfile:
+            json.dump(data, outfile)
+
+
+# write a csv file
+def write_csv_file(bucket_name, file_name, data, type='s3'):
+    if(type == 's3'):
+        csv_buffer = StringIO()
+        data.to_csv(csv_buffer, index=False)
+        cf.S3_CLIENT.put_object(Bucket=bucket_name, Key=file_name, Body = csv_buffer.getvalue())
+    elif(type == 'local'):
+        data.to_csv(file_name, index=False)
+
+
+# read a csv file
+def read_csv_file(bucket_name, file_name, type='s3'):
+    if(type == 's3'):
+        data = cf.S3_CLIENT.get_object(Bucket=cf.S3_DATA_PATH, Key=file_name)
+        data = pd.read_csv(data['Body'])
+    elif(type == 'local'):
+        data = pd.read_csv(file_name)
+    return data
 
 
 def split_data(X, y, test_size=0.2, random_state=0):
